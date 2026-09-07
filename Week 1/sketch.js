c4a48d42eda9167f396bd9ddd219aa4490828ed1
pjomander-cmyk <pjomander@gmail.com>
2026-09-07 11:13:20 +0200

    Update sketch.js
    javascript  tot en met nummer 2 eindopdracht week 1

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -5,3 +5,19 @@ function setup() {
 function draw() {
   background(220);
 }
+function setup( ) {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill('red')
+  text("niels", 10,10,100)
+rect(10,70,100,20);
+fill('white')
+  rect (10,90,100, 20);
+  fill('blue')
+  rect (10,110,100,20);
+  fill ('black')
+  text ("1" , 4 ,10,900)
+  text ("2" , 4 ,70,900)
