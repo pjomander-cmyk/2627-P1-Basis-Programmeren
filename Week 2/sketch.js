9b25077b969d259bb6640125e2b564414b3a9b59
pjomander-cmyk <pjomander@gmail.com>
2026-09-10 12:37:07 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,10 +1,21 @@
-function setup() {
+
+function setup( ) {
   createCanvas(800, 600);
 }
 
 function draw() {
-  background	(
-  );
+  background(135,206,235)
+  stroke(220) 
+
+    
+  
+; //dit zijn de statische elementen 
+  fill('gray') 
+  rect (1,425,800,500); 
+  fill('green')
+  rect (1,410,800,15)
+
+ 
+ 
+
 }
-
-
