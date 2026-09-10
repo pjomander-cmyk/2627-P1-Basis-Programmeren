082909201b398b431d6268737da2d102d0f7c5e9
pjomander-cmyk <pjomander@gmail.com>
2026-09-10 15:24:14 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -4,16 +4,28 @@ function setup( ) {
 }
 
 function draw() {
-  background(135,206,235)
+  background(135,206,235 )
   stroke(220) 
 
     
   
 ; //dit zijn de statische elementen 
+// de weg
   fill('gray') 
   rect (1,425,800,500); 
+
+
+
+//het stuk groen tussen de weg en de bergen 
   fill('green')
-  rect (1,410,800,15)
-  fill('gray') 
-  triangle
+  rect (1,410,800,15)  
+
+
+// de bergen
+  fill(128,128,128)
+  triangle(50,410,190,410,120,290)  
+  triangle( 192,410,450,410,305,200)
+  triangle (490,410,670,410,560,100)
+
+
 }
