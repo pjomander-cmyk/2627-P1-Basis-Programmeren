807b894d2035244d4138a345cd4a926b231a5ee4
pjomander-cmyk <pjomander@gmail.com>
2026-09-21 10:58:53 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,50 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let muisxvak1 = 0; 
+let muisyvak1 = 0 ; 
+let muisxvak2 
+let muisyvak2 
+let muisxvak3
+let muisyvak3
+let muisxvak4
+let muisyvak4
+let muisxvak5
+let muisyvak5
+let muisxvak6 
+let muisyvak6
+let muisxvak7
+let muisyvak7
+let muisxvak8
+let muisyvak8 
+let muisxvak9
+let muisyvak9
 
+function setup() { 
+  createCanvas(600, 600);
+}
 function draw() {
-  background(220);
+  background(0,255,255);
+
+//bordjes van het bord statiche elementen
+
+fill("black") 
+square (125,110,350)
+
+fill("255,255,255")
+strokeWeight(0)
+square(140,130,100 ) 
+square(140,235,100 ) 
+square(140,340,100 ) 
+//2de rij
+square(250,130,100 )  
+square(250,235,100 ) 
+square(250,340,100 )
+//3de rij 
+square(360,130,100 ) 
+square(360,235,100 ) 
+square(360,340,100 ) 
+// einde van de statishe elementen van het bord
+fill("black") 
+text(muisxvak1,100,100)
+
+
+
 }
